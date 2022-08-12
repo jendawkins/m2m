Fixed issue with e_met and pi_met initialization, removed variance in forward pass, model seems to be working great, only issue is shrinking r_met values to basically zero which doesn't affect learning when we have a set number of clusters, but does seem to when are learning the number of clusters

local_dispatcher_MAP.py
@@ -18,20 +18,22 @@ args = parser.parse_args()
 
 max_load = args.max_load
 
-param_dict = {('L', 'K'): [(10,10)], ('L_true', 'K_true'): [(2,2)],
+param_dict = {('L', 'K', 'lm', 'lb'): [(10,10,1,1)], ('L_true', 'K_true'): [(2,2)],
               'seed': [0,1,2,3,4,5],
               'iter': 20000,
-              'lr': [0.01], 'meas_var': 0.010,
+              'lr': [0.001], 'meas_var': 0.010,
               'data': 'synthetic',
-              'linear': 1, 'w_tau': [(-0.1, -1)],
-              'a_tau': [(-0.1, -3)],
-              'adjust_lr': [0,1],
-              'lm': [1], 'lb': [1],
+              'linear': 0, 'nltype': 'poly', 'w_tau': [(-0.1, -1.5)],
+              'a_tau': [(-0.1, -2.5)],
+              'adjust_lr': [1],
+              # 'lm': [1], 'lb': [1],
               'locs': ['true'], 'N_met': 40, 'N_bug': 40,
               # 'case': 'new_data_gen'
               }
 
 
+if args.case:
+    param_dict['case'] = args.case
 total_iters = np.prod([len(v) for v in param_dict.values() if hasattr(v, '__len__') and not isinstance(v, str)])
 print(total_iters)
 

model.py
@@ -178,6 +178,7 @@ class Model(nn.Module):
             mu = np.log(loc)
             var = np.log(scale)
             self.distributions['r_met'] = LogNormal(torch.tensor(mu), scale = torch.sqrt(torch.tensor(var)))
+            # self.distributions['r_met'] = HalfNormal(torch.tensor(10.0))
 
             # We define the prior for mu_met to be a multivariate normal with mean 0 and variance 100 (since the input
             # locations are already scaled to have variance 1)
@@ -204,6 +205,7 @@ class Model(nn.Module):
             var = np.log(scale)
             self.distributions['r_bug'] = LogNormal(torch.tensor(mu), scale = torch.sqrt(torch.tensor(var)))
 
+            # self.distributions['r_bug'] = HalfNormal(torch.tensor(10.0))
             self.distributions['mu_bug'] = MultivariateNormal(torch.zeros(self.bug_embedding_dim),
                                                               1e4*torch.eye(self.bug_embedding_dim))
 
@@ -213,7 +215,10 @@ class Model(nn.Module):
 
         # alpha is parameterized by a binary concrete with loc=1/(K*L) (defined above) and self.alpha_temp, which
         # decreases throughout learning like self.omega_temp
-        self.alpha_loc = 0.5/L
+        if self.learn_num_bug_clusters:
+            self.alpha_loc = 0.5/L
+        else:
+            self.alpha_loc = 1/L
         self.distributions['alpha'] = BinaryConcrete(self.alpha_loc, self.alpha_temp)
 
         # e_met is only learned (and thus the prior only used) if we are learning the number of metabolite clusters;
@@ -302,9 +307,9 @@ class Model(nn.Module):
             self.r_met = nn.Parameter(torch.log(torch.Tensor(np.array(r))), requires_grad=True)
             self.z_act = torch.Tensor(get_one_hot(kmeans.labels_, l = self.K))
             if self.learn_num_met_clusters:
-                self.e_met = nn.Parameter(torch.Tensor(np.array(gp_size)), requires_grad=True)
+                self.e_met = nn.Parameter(torch.log(torch.Tensor(np.array(gp_size)/self.N_met)), requires_grad=True)
             else:
-                self.e_met = torch.Tensor(np.array(gp_size))
+                self.e_met = torch.log(torch.Tensor(np.array(gp_size)/self.N_met))
             self.pi_met = nn.Parameter(torch.log(torch.Tensor(np.array(gp_size)/self.N_met).unsqueeze(0)), requires_grad=True)
         else:
             if self.learn_num_met_clusters:
@@ -346,8 +351,8 @@ class Model(nn.Module):
     def forward(self, x, y):
         # Forward function, contains all the model equations and calls MAP_loss.py to calculate loss
         # Omega and alpha epsilon are to keep w and alpha from getting to close to 0 or 1 and causing numerical issues
-        omega_epsilon = 1e-13
-        alpha_epsilon = self.alpha_temp / 4
+        omega_epsilon = self.omega_temp / 10
+        alpha_epsilon = self.alpha_temp / 10
         if self.microbe_locs is not None:
             kappa = torch.stack(
                 [torch.sqrt(((self.mu_bug - torch.tensor(self.microbe_locs[m, :])).pow(2)).sum(-1)) for m in
@@ -365,8 +370,8 @@ class Model(nn.Module):
         self.alpha_act = (1-2*alpha_epsilon)*torch.sigmoid(self.alpha/self.alpha_temp) + alpha_epsilon
 
         if self.linear:
-            out_clusters = self.beta[0,:] + torch.matmul(bn, self.beta[1:,:]*self.alpha_act) + \
-                           Normal(0,torch.sqrt(torch.exp(self.sigma))).sample([bn.shape[0], self.K])
+            out_clusters = self.beta[0,:] + torch.matmul(bn, self.beta[1:,:]*self.alpha_act)
+                           # + Normal(0,torch.sqrt(torch.exp(self.sigma))).sample([bn.shape[0], self.K])
 
         else:
             out_clusters = self.nam(bn, self.alpha_act, self.beta, self.sigma)

plot_helper.py
@@ -133,10 +133,8 @@ def plot_syn_data(path, x, y, g, gen_z, gen_bug_locs, gen_met_locs,
     fig2.savefig(path + 'cluster_histogram.png')
     plt.close(fig2)
 
-    try:
-        bug_active,met_active = np.where((gen_alpha * gen_beta[1:,:])>= 1e-6)
-    except:
-        bug_active, met_active = np.where((gen_alpha) >= 1e-6)
+    bug_active = np.where(gen_alpha.sum(1) >= 1e-6)[0]
+    met_active = np.where(gen_alpha.sum(0) >= 1e-6)[0]
 
     K = gen_z.shape[1]
     L = gen_u.shape[1]
@@ -148,6 +146,7 @@ def plot_syn_data(path, x, y, g, gen_z, gen_bug_locs, gen_met_locs,
     # Microbe clusters vs metabolite cluster
     for bug_clust, met_clust in zip(bug_active, met_active):
         ixs = np.where(gen_z[:, met_clust] == 1)[0]
+        # ix_bug = np.where(gen_u[:, bug_clust]==1)[0]
         for ix in ixs:
             ax[ii].scatter(g[:,bug_clust], y[:, ix])
         ax[ii].set_xlabel('Microbe sum of cluster ' + str(bug_clust))
@@ -156,12 +155,13 @@ def plot_syn_data(path, x, y, g, gen_z, gen_bug_locs, gen_met_locs,
         slope = np.round((np.max(y[:, ixs[0]]) - np.min(y[:, ixs[0]])) / ((np.max(g[:, bug_clust]) - np.min(g[:, bug_clust]))), 3)
         ax[ii].text(0.6, 0.8, 'slope = ' + str(slope), horizontalalignment='center',
                       verticalalignment='center', transform=ax[ii].transAxes)
-        ii += 1
         try:
             ax[ii].text(0.6, 0.6, 'beta = ' + str(gen_beta[bug_clust + 1, met_clust]), horizontalalignment='center',
                           verticalalignment='center', transform=ax[ii].transAxes)
         except:
+            ii += 1
             continue
+        ii += 1
     fig.tight_layout()
     fig.savefig(path + '-sum_x_v_y.png')
     plt.close(fig)
@@ -351,9 +351,10 @@ def plot_param_traces(path, param_dict, true_vals, fold):
             else:
                 for j in range(nn):
                     new_k, new_j = k, j
-                    # if 'r_' in name:
-                    #     trace = [np.exp(p).squeeze()[new_k, new_j] for p in plist]
-                    trace = [p.squeeze()[new_k, new_j] for p in plist]
+                    if 'r_' in name:
+                        trace = [np.exp(p).squeeze()[new_k, new_j] for p in plist]
+                    else:
+                        trace = [p.squeeze()[new_k, new_j] for p in plist]
                     if 'alpha' in name or 'w' in name or 'z' in name:
                         mindat = -0.05
                         maxdat = 1.05
@@ -746,8 +747,10 @@ def plot_loss_dict(path, fold, loss_dict):
         ax[i].set_xlabel('Iterations')
         ax[i].set_ylabel('Loss')
         ax[i].set_title(param)
-        if param == 'y':
+        try:
             ax[i].set_yscale('log')
+        except:
+            continue
     fig.savefig(path + 'seed' + str(fold) + '_loss_dict.png')
 
 